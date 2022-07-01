0205a1653a0b750c9b4bb0dbcdd1aba999105e1b
Christopher James hill <christopher@nanonino.com>
2022-07-01 10:09:06 +0200

    Changed image directory for meta images

--- composables/metatags.ts
+++ composables/metatags.ts
@@ -1,8 +1,8 @@
 const description =
     "Starting today, you can pre-register your name to secure your handle. Choose wisely and start receiving exclusive benefits from Tonto!";
 const title = "Pre-Register | Secure your Handle | Get Start with Tonto";
-const primaryUrl = "https://start.gettonto.com";
-const metaTagImage = "https://start.gettonto.com/secureYourHandleAtTonto.png";
+const primaryUrl = "https://www.gettonto.com/start";
+const metaTagImage = "https://s.gettonto.com/secureYourHandleAtTonto.png";
 export function useMetaTags() {
     return { description, title, primaryUrl, metaTagImage };
 }
