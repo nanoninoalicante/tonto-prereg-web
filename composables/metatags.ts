5c71a5c73302e0dc65ddacee432835174ca3abc2
Christopher James hill <christopher@nanonino.com>
2022-07-01 13:05:48 +0200

    Updated sitemap, public directory

--- composables/metatags.ts
+++ composables/metatags.ts
@@ -1,9 +1,8 @@
 const description =
     "Starting today, you can pre-register your name to secure your handle. Choose wisely and start receiving exclusive benefits from Tonto!";
 const title = "Pre-Register | Secure your Handle | Get Start with Tonto";
-const primaryUrl = "https://www.gettonto.com/start";
-const metaTagImage =
-    "https://s.gettonto.com/_prereg-public/secureYourHandleAtTonto.png";
+const primaryUrl = "https://start.gettonto.com/";
+const metaTagImage = "https://start.gettonto.com/secureYourHandleAtTonto.png";
 export function useMetaTags() {
     return { description, title, primaryUrl, metaTagImage };
 }
