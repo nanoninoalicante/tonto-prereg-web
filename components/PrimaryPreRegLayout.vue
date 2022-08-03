a75e5e05e1f2946256ff2068f3b134672e0f3f4d
Christopher Hill <cjameshill@gmail.com>
2022-08-03 21:03:56 +0000

    hide floating

--- components/PrimaryPreRegLayout.vue
+++ components/PrimaryPreRegLayout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { watch, onMounted } from "vue";
+import { reactive, ref, watch, computed } from "vue";
 import FloatingChooseYourHandle from "~/components/FloatingChooseYourHandle";
 import ChooseHandleForm from "~/components/ChooseHandleForm";
 import PrimarySection from "~/components/PrimarySection";
@@ -7,19 +7,57 @@ import PrimaryPageHolder from "~/components/PrimaryPageHolder";
 import FullPageWalkThrough from "~/components/FullPageWalkThrough";
 import ArrowDownIconV2 from "~/components/icons/ArrowDownIconV2";
 import PageArrowHolder from "./PageArrowHolder";
-import { useScroll } from "@vueuse/core";
-import { useElementVisibility } from "@vueuse/core";
+import { useScroll, useElementBounding } from "@vueuse/core";
 const el = ref(null);
 const lastSection = ref(null);
-const { x, y, isScrolling, arrivedState, directions } = useScroll(el);
-const targetIsVisible = useElementVisibility(lastSection);
+const { y } = useScroll(el);
+const { y: sectionY, height } = useElementBounding(lastSection);
+const hideFloatingForm = ref(true);
+watch(y, (newval) => {
+    if (newval + height.value / 2 > sectionY.value) {
+        hideFloatingForm.value = false;
+    } else {
+        hideFloatingForm.value = true;
+    }
+});
+
+const images = ref([
+    "cheerful-young-lady-wearing-headphones-using-mobi-2021-12-09-08-12-41-utc.jpg",
+    "exercises-stretching-body-muscles-in-city-healthy-2021-09-02-07-22-45-utc.jpg",
+    "happy-afro-man-using-mobile-smartphone-outdoor-2021-09-09-16-19-15-utc.jpg",
+    "happy-man-at-home-smiling-and-listening-to-music-f-2022-07-26-05-17-25-utc.jpg",
+    "happy-spanish-girl-in-airpods-is-singing-with-mobi-2022-06-06-23-53-18-utc.jpg",
+    "modern-outdoor-workout-healthy-lifestyle-and-star-2021-09-02-17-13-06-utc.jpg",
+    "outdoor-shot-of-caucasian-female-enjoys-listening-2021-08-30-23-54-36-utc.jpg",
+    "pretty-caucasian-woman-in-wireless-headphones-usin-2021-12-09-08-17-52-utc.jpg",
+    "smiling-girl-using-music-app-on-phone-2021-09-24-03-50-37-utc.jpg",
+    "smiling-young-business-woman-using-her-free-hands-2021-08-27-09-31-39-utc.jpg",
+    "young-woman-listening-music-smart-phone-drinking-c-2022-02-03-16-33-07-utc.jpg",
+]);
+const walkthroughScreens = reactive([
+    {
+        text: "AUDIO SOCIAL",
+    },
+    {
+        text: "AUDIO SOCIAL",
+    },
+    {
+        text: "AUDIO SOCIAL",
+    },
+    {
+        text: "AUDIO SOCIAL",
+    },
+]);
 </script>
 <template>
     <PrimaryPageHolder ref="el">
-        <div class="fixed z-50 text-xl text-red-600">{{ targetIsVisible }}</div>
-        <FloatingChooseYourHandle></FloatingChooseYourHandle>
-        <PrimarySection class="hidden bg-[url('/socialNetwork.png')] md:block">
-            <FullPageWalkThrough>AUDIO SOCIAL</FullPageWalkThrough>
+        <FloatingChooseYourHandle
+            v-if="hideFloatingForm"
+        ></FloatingChooseYourHandle>
+        <PrimarySection :class="`hidden bg-[url('/${images[0]}')] md:block`">
+            <FullPageWalkThrough>{{
+                walkthroughScreens[0].text
+            }}</FullPageWalkThrough>
             <PageArrowHolder />
         </PrimarySection>
 
@@ -27,29 +65,26 @@ const targetIsVisible = useElementVisibility(lastSection);
             <slot></slot>
             <PageArrowHolder class="hidden md:hidden" />
         </PrimarySection>
-        <PrimarySection class="block bg-[url('/socialNetwork.png')] md:hidden">
-            <FullPageWalkThrough>AUDIO SOCIAL</FullPageWalkThrough>
-
+        <PrimarySection
+            v-for="(screen, index) in walkthroughScreens"
+            :key="index"
+        >
+            <FullPageWalkThrough>{{ screen.text }}</FullPageWalkThrough>
             <PageArrowHolder />
-        </PrimarySection>
-        <PrimarySection class="bg-[url('/onlyAudios.png')]">
-            <FullPageWalkThrough>ONLY AUDIOS</FullPageWalkThrough>
-            <PageArrowHolder />
-        </PrimarySection>
-        <PrimarySection class="bg-[url('/freeSpeech.png')]">
-            <FullPageWalkThrough>FREE SPEECH</FullPageWalkThrough>
-            <PageArrowHolder />
-        </PrimarySection>
-        <PrimarySection class="bg-[url('/noLimits.png')]">
-            <FullPageWalkThrough>NO LIMITS</FullPageWalkThrough>
-            <PageArrowHolder />
+            <template v-slot:bgimage>
+                <img
+                    class="absolute pointer-events-none z-0 top-0 left-0 h-full w-full object-left object-cover"
+                    :src="'/' + images[index]"
+                    alt=""
+                />
+            </template>
         </PrimarySection>
         <PrimarySection ref="lastSection" class="block md:hidden">
             <ChooseHandleForm
                 ><h1
                     class="text-left text-5xl font-medium tracking-tighter text-grey-700"
                 >
-                    PRE REGISTER {{ y }}
+                    PRE REGISTER
                 </h1>
                 <h1
                     class="text-left text-6xl font-bold tracking-tighter text-grey-700"
--- components/PrimarySection.vue
+++ components/PrimarySection.vue
@@ -3,9 +3,10 @@ import PrimaryLogoHolder from "~/components/PrimaryLogoHolder";
 </script>
 <template>
     <section
-        class="items-left relative flex min-h-screen overflow-x-hidden w-full snap-starts flex-col justify-start bg-cover px-4 py-4 md:w-1/2"
+        class="relative items-left flex min-h-screen overflow-x-hidden w-full snap-start flex-col justify-start bg-cover px-4 py-4 md:w-1/2"
     >
-        <div class="flex flex-col w-full">
+        <slot name="bgimage"></slot>
+        <div class="relative h-full flex flex-col w-full">
             <div class="flex px-6 pt-8">
                 <PrimaryLogoHolder />
             </div>
