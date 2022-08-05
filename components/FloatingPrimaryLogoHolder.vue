8ca548236f269a298a5d72aeeade9c71fe0f8b01
Christopher Hill <cjameshill@gmail.com>
2022-08-05 12:11:24 +0000

    Updated logo positioning

--- components/FloatingPrimaryLogoHolder.vue
+++ components/FloatingPrimaryLogoHolder.vue
@@ -2,7 +2,7 @@
 import PrimaryLogo from "~/components/PrimaryLogo"
 </script>
 <template>
-  <div class="fixed top-0 left-14 z-[100]">
+  <div class="fixed top-0 left-0 px-8 md:px-8 z-[100]">
     <PrimaryLogo/>
   </div>
 </template>
