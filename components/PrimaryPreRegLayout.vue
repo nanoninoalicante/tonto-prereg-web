8c38990b70e14e05d3f27dc2a6dd745d1f31fbf9
Christopher Hill <cjameshill@gmail.com>
2022-08-05 09:58:41 +0000

    Adjusted desktop text padding bottom

--- components/PrimaryPreRegLayout.vue
+++ components/PrimaryPreRegLayout.vue
@@ -109,12 +109,12 @@ onMounted(() => {
         <PrimarySection ref="lastSection" class="block md:hidden">
             <ChooseHandleForm
                 ><h1
-                    class="text-left text-5xl font-medium tracking-tighter text-grey-700"
+                    class="text-left text-7xl font-medium tracking-tighter text-grey-700"
                 >
                     PRE REGISTER
                 </h1>
                 <h1
-                    class="text-left text-6xl font-bold tracking-tighter text-grey-700"
+                    class="text-left text-7xl font-bold tracking-tighter text-grey-700"
                 >
                     NOW
                 </h1></ChooseHandleForm
--- components/PrimarySection.vue
+++ components/PrimarySection.vue
@@ -6,7 +6,7 @@ import PrimaryLogoHolder from "~/components/PrimaryLogoHolder";
         class="relative items-left flex min-h-screen overflow-x-hidden w-full snap-start flex-col justify-start bg-cover px-4 py-4 md:w-1/2"
     >
         <slot name="bgimage"></slot>
-        <div class="relative h-full flex flex-col pt-36 md:pt-0 md:justify-end w-full">
+        <div class="relative h-full flex flex-col pt-36 md:pt-0 md:pb-20 md:justify-end w-full">
             <div class="flex py-4 px-4">
                 <slot />
             </div>
